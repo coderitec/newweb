06de4a13aad6c4683d106eb3819499ba421dfd33
Francis Enemuo <francenemuo@gmail.com>
2022-12-27 04:00:38 +0100

    feat:complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
 import OAuth from '../components/OAuth'
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { toast } from 'react-toastify'
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -11,6 +13,7 @@ export default function SignIn() {
   })
 
   const {email, password} = formData
+  const navigate = useNavigate()
 
   function onChange(e){
     setFormdata((prevState)=>({
@@ -18,6 +21,19 @@ export default function SignIn() {
       [e.target.id]: e.target.value
 
     }))
+  }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      if(userCredential.user){
+        navigator('/')
+      }
+    } catch (error) {
+      toast.error('Bad user credential')
+    }
   }
   return (
     <section>
@@ -29,7 +45,7 @@ export default function SignIn() {
           alt="secured house"  className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
               <input type="email" name="email" id="email" 
               className='w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
                value={email} onChange ={onChange} placeholder="Email address"/>
