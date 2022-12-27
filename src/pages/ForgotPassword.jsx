c383247c6f135805a58eb8b983594adf0754151e
Francis Enemuo <francenemuo@gmail.com>
2022-12-27 04:32:42 +0100

    feat:complete the forgot password page functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,13 +1,29 @@
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import OAuth from '../components/OAuth'
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("")
-
+  const navigate = useNavigate()
+  
   function onChange(e){
     setEmail(e.target.value)
   }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success('Email confirmation sent, you can now reset')
+      navigate('/')
+    } catch (error) {
+      toast.error('Could not send Reset password')
+    }
+  }
+
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Forgot Password</h1>
@@ -18,7 +34,8 @@ export default function ForgotPassword() {
           alt="secured house"  className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          
+          <form onSubmit={onSubmit}>
          
               <input type="email" name="email" id="email" 
               className='w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
